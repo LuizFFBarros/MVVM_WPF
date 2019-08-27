9cd9ed9cdffdf8559aa407d727e84095efc8b475
luiz.barros@squadra.com.br <luiz.barros@squadra.com.br>
2019-08-27 18:59:12 -0300

    Add project files.

---
+++ DeletarCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFMVVMCRUD.ViewModel;
+
+namespace WPFMVVMCRUD
+{
+    public class DeletarCommand : BaseCommand
+    {
+        public override bool CanExecute(object parameter)
+        {
+            var viewModel = parameter as FuncionariosViewModel;
+            return viewModel != null && viewModel.FuncionarioSelecionado != null;
+        }
+
+        public override void Execute(object parameter)
+        {
+            var viewModel = (FuncionariosViewModel)parameter;
+            viewModel.Funcionarios.Remove(viewModel.FuncionarioSelecionado);
+            viewModel.FuncionarioSelecionado = viewModel.Funcionarios.FirstOrDefault();
+        }
+    }
+}
---
+++ ViewModel/FuncionariosViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFMVVMCRUD.ViewModel
+{
+    public class FuncionariosViewModel : BaseNotifyPropertyChanged
+    {
+        public ObservableCollection<Model.Funcionario> Funcionarios { get; private set; }
+        public DeletarCommand Deletar { get; private set; } = new DeletarCommand();
+        public NovoCommand Novo { get; private set; } = new NovoCommand();
+        public EditarCommand Editar { get; private set; } = new EditarCommand();
+
+
+        private Model.Funcionario _funcionarioSelecionado;
+        public Model.Funcionario FuncionarioSelecionado
+        {
+            get { return _funcionarioSelecionado; }
+            set
+            {
+                SetField(ref _funcionarioSelecionado, value);
+                Deletar.RaiseCanExecuteChanged();
+                Editar.RaiseCanExecuteChanged();
+            }
+        }
+
+        public FuncionariosViewModel()
+        {
+            Funcionarios = new ObservableCollection<Model.Funcionario>();
+            Funcionarios.Add(new Model.Funcionario()
+            {
+                Id = 1,
+                Nome = "André",
+                Sobrenome = "Lima",
+                DataNascimento = new DateTime(1984, 12, 31),
+                Sexo = Model.Sexo.Masculino,
+                EstadoCivil = Model.EstadoCivil.Casado,
+                DataAdmissao = new DateTime(2010, 1, 1)
+            });
+
+            FuncionarioSelecionado = Funcionarios.FirstOrDefault();
+        }
+    }
+}
